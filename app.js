9f6b8463496992a1e64856c3fccdf65970764632
shabbu akhtar <shabbuten55@gmail.com>
2023-01-31 16:53:15 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//rest oprater
+		// array destucturing
+
+		let arr= ["dastgir",20,"male",["urdu middle",7]];
+
+
+		let [name,age,gender,[school,std]] = arr;
+
+
+		// let name = arr[0];
+		// let age = arr[1];
+		// let gender = arr[2];
+
+		console.log(name);
+		console.log(age);
+		console.log(gender);
+		console.log(school);
+		console.log(std);
